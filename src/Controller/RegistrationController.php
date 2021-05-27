be501f49e2f7dba00526aa405b78d6e44c594e4d
DuongDucThangHaNoi312 <thangkmact2@gmail.com>
2021-05-27 22:35:42 +0700

    updateFormUser

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,36 @@ class RegistrationController extends AbstractController
         ]);
     }
 }
-
-
-
-
+//public function registerAction(Request $request)
+//{
+//    // Create a new blank user and process the form
+//    $user = new User();
+//    $form = $this->createForm(UserType::class, $user);
+//    $form->handleRequest($request);
+//
+//    if ($form->isSubmitted() && $form->isValid()) {
+//        // Encode the new users password
+//        $encoder = $this->get('security.password_encoder');
+//        $password = $encoder->encodePassword($user, $user->getPlainPassword());
+//        $user->setPassword($password);
+//
+//        // Set their role
+//        $user->setRole('ROLE_USER');
+//
+//        // Save
+//        $em = $this->getDoctrine()->getManager();
+//        $em->persist($user);
+//        $em->flush();
+//
+//        return $this->redirectToRoute('login');
+//    }
+//
+//    return $this->render('auth/register.html.twig', [
+//        'form' => $form->createView(),
+//    ]);
+//}
+//}
+//
+//
+//
+//
---
+++ src/Form/UserType.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Form;
+
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\User',
+        ]);
+    }
+}
